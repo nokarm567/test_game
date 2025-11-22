b1b48e8614a3ce95a2af9a8bde12c9ef4423a615
nokarm <nokarm567@gmail.com>
2025-11-22 17:57:44 +0100

    test

--- src/main.rs
+++ src/main.rs
@@ -3,7 +3,7 @@ use macroquad::prelude::*;
 #[macroquad::main("My game")]
 async fn main() {
     loop {
-        clear_background(DARKPURPLE);
+        clear_background(BLUE);
         next_frame().await
     }
 }
